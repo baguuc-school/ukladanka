63722368644677956d40509ee2588849206537d4
Jan Tadeusz Żukowski <4886712@teb-gdansk.ad>
2026-02-18 12:14:40 +0100

    initial commit

---
+++ Ukladanka/App.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace Ukladanka
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+
+}
